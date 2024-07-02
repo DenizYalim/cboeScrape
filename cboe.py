59c7466f7c9d62f4e2394176e2a892bbfe276c80
DenizYalim <116675869+DenizYalim@users.noreply.github.com>
2024-07-02 03:32:11 +0300

    Add files via upload

--- cboe.py
+++ cboe.py
@@ -15,28 +15,18 @@ driver.get('https://www.cboe.com/tradable_products/vix/vix_futures/')
 source = driver.page_source
 
 allRowsHtml = []
-
 def getLines(very_long_string, starter, ender):
     testString = starter + '(.*?)' + ender
     matches = re.findall(testString, very_long_string)
-    for match in matches:       #TODO: We probably don't need to create matches and go though it again, just write it inside it
-        arr2 = []
-        arr2.append(''.join(match))
-        #print(match)
-    return arr2
+    return matches
 
 allRowsHtml = getLines(source, 'tr role="row"', "</tr>")
 
-allRows = []
-def getKeys(starter_data, ender_data):
-    for i in allRowsHtml:
-        allRows = getLines(i, starter_data, ender_data)
-        print(allRows)
+table_2d = []
+for row in allRowsHtml:
+    table_2d.append(getLines(row, 'fOvMUL">', "</div>"))
 
-    pass
-
-getKeys('fOvMUL">', "</div>")
-
+print(table_2d)
 
 print("\n")
 driver.quit()
