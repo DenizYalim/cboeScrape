b0a2499c12bbddd6d68b77f91a307e913d7ccd1f
DenizYalim <116675869+DenizYalim@users.noreply.github.com>
2024-07-02 03:10:27 +0300

    Add files via upload

--- cboe.py
+++ cboe.py
@@ -7,20 +7,36 @@ from selenium.webdriver.chrome.options import Options
 print("Start cboe tablo çekme")
 
 options = Options()  #webdriver.ChromeOptions()
-
+options.add_argument("--headless")
 driver = webdriver.Chrome(options=options)
+
 driver.get('https://www.cboe.com/tradable_products/vix/vix_futures/')
 
 source = driver.page_source
 
+allRowsHtml = []
 
-def getKeys(very_long_string, starter, ender):
+def getLines(very_long_string, starter, ender):
     testString = starter + '(.*?)' + ender
     matches = re.findall(testString, very_long_string)
-    print(matches)
+    for match in matches:       #TODO: We probably don't need to create matches and go though it again, just write it inside it
+        arr2 = []
+        arr2.append(''.join(match))
+        #print(match)
+    return arr2
 
+allRowsHtml = getLines(source, 'tr role="row"', "</tr>")
 
-getKeys(source, 'fOvMUL">', "</div>")
+allRows = []
+def getKeys(starter_data, ender_data):
+    for i in allRowsHtml:
+        allRows = getLines(i, starter_data, ender_data)
+        print(allRows)
+
+    pass
+
+getKeys('fOvMUL">', "</div>")
+
 
 print("\n")
 driver.quit()
